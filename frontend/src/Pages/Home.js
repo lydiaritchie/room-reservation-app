5c80fc339d50cbb37b786c02ba7bc31c0a1f7cd3
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2025-06-13 11:18:43 -0500

    set up app.js and index.js with routes

---
+++ frontend/src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+/** Home page that displays the public calendar. */
+
+function Home(){
+    return (
+        <div>
+            <h1>Home Page</h1>
+            sdfsd;lkj
+        </div>
+
+    )
+}
+
+export default Home;
