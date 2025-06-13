ed6459defa9119585a05e2c79b874301c38bb27f
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2025-06-13 10:55:55 -0500

    Started app.js and server.js, installed cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,22 @@
-require('dotenv').config(); // Loads .env variables
+const { PORT = 5001 } = process.env;
+
+const app = require("./app");
+const knex = require("./db/connection");
+
+/*knex.migrate
+  .latest()
+  .then((migrations) => {
+    console.log("migrations", migrations);
+    app.listen(PORT, listener);
+  })
+  .catch((error) => {
+    console.error(error);
+    knex.destroy();
+  });
+  */
+
+  app.listen(PORT, listener);
+
+function listener() {
+  console.log(`Listening on Port ${PORT}!`);
+}
