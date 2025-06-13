4d0311783b941d79fcd61c584c1e64dda2943f13
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2025-06-13 13:58:43 -0500

    Created routes and basic page templates

---
+++ frontend/src/Pages/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+/** Landing page for users to login to their accounts. */
+
+function Login(){
+    return (
+        <div>
+            <div className="min-vh-100" style={{backgroundColor: "#D5EDDC"}}>
+            Login
+            </div>
+        </div>
+    )
+}
+
+export default Login;
---
+++ frontend/src/pages/ForgotPassword.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+/** Page you go to to reset your password. */
+
+function ForgotPassword(){
+    return (
+        <div>
+            <div className="min-vh-100" style={{backgroundColor: "#D5EDDC"}}>
+            Forgot password
+            </div>
+        </div>
+    )
+}
+
+export default ForgotPassword;
