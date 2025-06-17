04a36cdcf62816cee38495867d9c1a3197dce6de
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2025-06-17 16:36:00 -0500

    added connection.js and modified knexfile

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -6,42 +6,30 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    client: 'postgres',
+    connection: DATABASE_URL_DEVELOPMENT,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    debug: !!DEBUG,
+  },
+    production: {
+    client: 'postgres',
+    connection: DATABASE_URL_PRODUCTION,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    debug: !!DEBUG,
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+
+
 
 };
