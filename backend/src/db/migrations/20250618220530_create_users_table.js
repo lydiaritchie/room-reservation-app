8bc7eeb18b6dc9b483e954fe82749e22d0c769a8
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2025-06-18 17:07:58 -0500

    Create migration file for users table

---
+++ backend/src/db/migrations/20250618220530_create_users_table.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('email').notNullable().unique(); // Email as username
+    table.string('first_name').notNullable();
+    table.string('last_name').notNullable();
+    table.text('password_hash').notNullable(); // For Argon2 hashes
+    table.timestamps(true, true); // created_at, updated_at
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('users');
+};
